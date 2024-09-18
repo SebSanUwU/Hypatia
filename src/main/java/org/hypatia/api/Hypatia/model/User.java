b05d282a203bb3d3e9690392568b97be169af390
unknown <christian.duarte@labinfo.is.escuelaing.edu.co>
2024-09-18 09:46:11 -0500

    First Commit

---
+++ src/main/java/org/hypatia/api/Hypatia/model/User.java
@@ -0,0 +1,9 @@
+package org.hypatia.api.Hypatia.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+}
